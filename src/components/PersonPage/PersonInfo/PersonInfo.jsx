68bf37cdd77c238846206de1483c253632c6dee1
dibrugh <dibrugh@gmail.com>
2023-12-26 18:21:29 +0300

    feat: create PersonInfo and PersonPhoto components

---
+++ src/components/PersonPage/PersonInfo/PersonInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./PersonInfo.module.css";
+
+const PersonInfo = ({ personInfo }) => {
+	return (
+		<>
+			<div>
+				<ul>
+					{personInfo.map(
+						({ title, data }) =>
+							data && (
+								<li key={title}>
+									<span>
+										{title}: {data}
+									</span>
+								</li>
+							)
+					)}
+				</ul>
+			</div>
+		</>
+	);
+};
+
+PersonInfo.propTypes = {
+	personInfo: PropTypes.array,
+};
+
+export default PersonInfo;
--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useParams } from "react-router";
 
+import { withErrorApi } from "@hoc-helpers/withErrorApi";
+
+import PersonInfo from "@components/PersonPage/PersonInfo";
+import PersonPhoto from "@components/PersonPage/PersonPhoto";
+import { getApiResource } from "@utils/network";
+import { API_PERSON } from "@constants/api";
+
+import { getPeopleImage } from "@services/getPeopleData";
+
 import styles from "./PersonPage.module.css";
 
-const PersonPage = () => {
+const PersonPage = ({ setErrorApi }) => {
+	const [personInfo, setPersonInfo] = useState(null);
+	const [personName, setPersonName] = useState(null);
+	const [personPhoto, setPersonPhoto] = useState(null);
+
 	const { id } = useParams();
-	console.log(id);
-	return <div>PersonPage{id}</div>;
+	useEffect(() => {
+		/* Т.е getApiResource асинхронная, для удобства можно использовать асинхронную IEF */
+		(async () => {
+			const response = await getApiResource(`${API_PERSON}/${id}`);
+
+			if (response) {
+				setPersonInfo([
+					{ title: "Height", data: response.height },
+					{ title: "Mass", data: response.mass },
+					{ title: "Hair Color", data: response.hair_color },
+					{ title: "Skin Color", data: response.skin_color },
+					{ title: "Eye Color", data: response.eye_color },
+					{ title: "Birth Year", data: response.birth_year },
+					{ title: "Gender", data: response.gender },
+				]);
+				setPersonName(response.name);
+				setPersonPhoto(getPeopleImage(id));
+				/* response.films */
+
+				setErrorApi(false);
+			} else {
+				setErrorApi(true);
+			}
+		})();
+	}, [id]);
+
+	return (
+		<>
+			<h1>{personName}</h1>
+			<PersonPhoto personPhoto={personPhoto} personName={personName} />
+			{personInfo && <PersonInfo personInfo={personInfo} />}
+		</>
+	);
 };
 
-/* PersonPage.propTypes = {
+PersonPage.propTypes = {
 	setErrorApi: PropTypes.func,
-}; */
+};
 
-export default PersonPage;
+export default withErrorApi(PersonPage);
