c496542e37b73afd71376da540f346c9f26c0295
dibrugh <dibrugh@gmail.com>
2023-12-28 02:41:27 +0300

    feat: implement favorites page

--- src/containers/FavoritesPage/FavoritesPage.jsx
+++ src/containers/FavoritesPage/FavoritesPage.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import PeopleList from "@components/PeoplePage/PeopleList";
 
 import styles from "./FavoritesPage.module.css";
 
 const FavoritesPage = () => {
-    // Получаемым данные из store через useSelector
-    const storeData = useSelector(state => state.rootReducer);
-    console.log(storeData)
-	return <div>FavoritesPage</div>;
+	const [people, setPeople] = useState([]);
+	// Получаемым данные из store через useSelector
+	const storeData = useSelector((state) => state.rootReducer);
+	useEffect(() => {
+		const arr = Object.entries(storeData);
+		if (arr.length) {
+			/* Преобразовываем store в соответствии с компонентов PeopleList (т.е id, name, img) */
+			const res = arr.map((item) => {
+				return {
+					id: item[0],
+					/* или item[1].name, item[1].img, */
+					...item[1],
+				};
+			});
+			setPeople(res);
+		}
+	}, []);
+	return (
+		<div>
+			<h1 className="header__text">Favorites Page</h1>
+			{people.length ? (
+				<PeopleList people={people} />
+			) : (
+				<h2 className={styles.comment}>No data</h2>
+			)}
+		</div>
+	);
 };
 
 export default FavoritesPage;
